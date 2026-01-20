cb337260a099d0d4ae2116861b3c26fdcae19418
Tequam z <tequamzgold@gmail.com>
2026-01-20 19:28:48 +0300

    fixing

--- index.tsx
+++ index.tsx
@@ -7,7 +7,7 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
+// we don't know what will happen
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
